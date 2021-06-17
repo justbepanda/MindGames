c335fa8941eb42a1d836fb7df06ce36f3810bfc2
Yury Ustinov <justbepanda@gmail.com>
2021-06-17 07:24:18 +0100

    add command lint

--- src/cli.php
+++ src/cli.php
@@ -1,15 +1,5 @@
 <?php
-
 namespace brain\games\cli;
-
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
-
 
 use function cli\line;
 use function cli\prompt;
@@ -19,4 +9,13 @@ function welcome()
     line('Welcome to the Brain Games');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    return true;
+}
+
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
 }
